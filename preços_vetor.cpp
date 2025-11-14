383689fa8c57e3f03519f9d2d461eb0ec80e5798
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-14 17:25:25 -0300

    preços_vetor.cpp

---
+++ preços_vetor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string posiçoes[15] = {"23.71", "12.33", "15.99", "13.12", "19.78", "86.55", "54.44", "19.89", "65.34", "55.67", "12.21", "10.13", "5.99", "50.11", "34.67"};
+        cout << posiçoes[1];
+   
+    return 0;
+}
