f2018e690cc9ce81da79cb016ff55a1cffe3405a
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-14 17:24:24 -0300

    estrutura_vertor.cpp

---
+++ estrutura_vertor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+     string frutas_verduras[15] = {"Banana","Maçã","Cenoura","Brócolis","Laranja","Batata","Uva","Alface","Mamão","Abóbora","Pera","Mandioca","Abacaxi","Beterraba","Manga"};
+     
+     cout<< frutas_verduras[14];
+
+    return 0;
+}
