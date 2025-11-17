dbdd5f654d711a7a66fb460e4aa179a35a686b94
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 14:25:39 -0300

    estrutura_nome_vetor.cpp

---
+++ estrutura_nome_vetor.cpp
@@ -0,0 +1,8 @@
+    string aluno[11] = {"ana", "pablo","heitor","victor", "miguel", "luiz", "igor","leticia", "icaro", "fabiano","maria"};
+    float nota;
+    cout<<"qula é sua nota "<< aluno[5]<< endl;
+    cin>> nota;
+    cout<<aluno[5]<< " sua nota é "<< nota;
+
+    return 0;
+}
