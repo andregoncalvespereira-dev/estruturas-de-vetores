1b2ae1b69fd071543d828474b206ce0ab1b8f0d6
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 14:24:35 -0300

    Create preco_vetor.cpp

---
+++ preco_vetor.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    float preço_carro[7] = {3000000000,1400000000,374500,253750,319270,13000000,60000000};
+     
+    cout<< preço_carro[2];
+
+    return 0;
+}
