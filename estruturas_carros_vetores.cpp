982be9e6508b5534536274233ef2372e7031db5c
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 14:21:39 -0300

    estruturas_carros_vetores.cpp

---
+++ estruturas_carros_vetores.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string marca[7] = {"aston", "dodge", "mitsubishi", "subaru", "bmw", "ferrari", "audi"};
+        cout << marca[1];
+   
+    return 0;
+}
