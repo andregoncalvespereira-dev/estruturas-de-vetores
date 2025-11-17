db43b7b9c7cb789d12a94e46d1d3b7622726b850
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 16:38:50 -0300

    subitraçao_vetores.cpp

---
+++ subitraçao_vetores.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int a[4]={2,4,6,8};
+    int b[4]= {1,3,5,7};
+    int c[4];
+    
+    for(int i = 0; i <4 ; i++){
+        c[i]= a[i] - b[i];
+        cout<<c[i]<< " ";
+    }
+    return 0;
+}
