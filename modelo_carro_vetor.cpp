c48be33695c1fb8c2ee819f1a50cf30d44d250b5
André Gonçalves Pereira <andre.goncalves.pereira@escola.pr.gov.br>
2025-11-17 14:23:39 -0300

    modelo_carro_vetor.cpp

---
+++ modelo_carro_vetor.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string marca[7] = {"aston_valyre", "dodge_toreto", "mitsubishi_eclipse", "subaru_brz", "bmw_m4", "ferrari_812", "pagani_huayra"};
+        cout << marca[1];
+   
+    return 0;
+}
